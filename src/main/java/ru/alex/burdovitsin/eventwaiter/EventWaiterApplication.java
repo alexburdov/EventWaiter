8813edc8a3eec9f7734c3ccac4775b0254dad7e8
Alexander Burdovitsin <alex4rf@gmail.com>
2025-02-21 17:44:48 +0700

    Init Project

---
+++ src/main/java/ru/alex/burdovitsin/eventwaiter/EventWaiterApplication.java
@@ -0,0 +1,13 @@
+package ru.alex.burdovitsin.eventwaiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventWaiterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventWaiterApplication.class, args);
+    }
+
+}
